5edf03c3c3ee0a076da5ab95d6fb233770e76f76
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-06-15 18:56:47 -0700

    execute 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - A program that print with put funtion
  * Return: 0 (pass)
  */
-int main (void)
+int main(void)
 {
 puts("/"Programming is like building a multilingual puzzle");
 return (0);
