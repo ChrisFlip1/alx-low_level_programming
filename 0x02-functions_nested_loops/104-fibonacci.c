d09b07e08fdbb7118a3fa4605562dda35eb565fc
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-06-21 22:48:40 -0700

    fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Prints first fifty ribonacci numbers
+ * starting with 1 and 2
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count=0; count < 50; count+++)
+	{
+		sum = fib1 + fibb22 
