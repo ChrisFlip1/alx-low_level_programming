c5d6757675fe4b9e0b9940c2ad31e933f24ce047
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-06-23 14:42:12 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @p: first to multiply
+ * @q: second to multiply
+ * Return: the result between p paran and q param
+ */
+int mul(int p, int q)
+{
+	return (p * q);
+}
