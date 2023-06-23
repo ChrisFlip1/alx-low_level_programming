6822dc276ddae05b0c26c4524a5723064891c341
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-06-23 15:51:30 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,23 +2,23 @@
 
 /**
  * print_line - draws a straight line according to parameters
- * @w: The num of lines to draw
+ * @n: The num of lines to draw
  * Return: empty
  */
-void print_line(int w)
+void print_line(int n)
 {
 	int x;
 
-	if (w >= 0)
+	if (n >= 0)
 	{
-		_putchar('\w');
+		_putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < w; x++)
+		for (x = 0; x < n; x++)
 		{
 			_putchar(95);
 		}
-		_putchar('\w');
+		_putchar('\n');
 	}
 }
