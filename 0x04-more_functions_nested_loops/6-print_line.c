85e24229a794047fddbeef83fde421a0c16e4582
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-06-23 15:46:40 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line according to parameters
+ * @w: The num of lines to draw
+ * Return: empty
+ */
+void print_line(int w)
+{
+	int w;
+
+	if (w >= 0)
+	{
+		_putchar('\w');
+	}
+	else
+	{
+		for (x = 0; x < w; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\w');
+	}
+}
