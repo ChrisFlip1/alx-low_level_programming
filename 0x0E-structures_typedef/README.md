6f6b27c5622f3312ddc3da86208a4dd1890980e9
Richard Pettey-Agamatey <sacketeyrich@yahoo.com>
2023-07-18 17:22:09 -0700

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+REAME 0x0E. C - Structures, typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/*
+ * struct dog - a dog basic information
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
